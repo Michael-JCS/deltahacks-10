44c65b50ef14ab582392dc68b149237dc11d8ed6
Kevin Le <let36@mcmaster.ca>
2024-01-13 18:49:52 -0500

    added set_info function to set name, phone, email and linkedin

--- db/db.py
+++ db/db.py
@@ -6,18 +6,26 @@ r = redis.Redis(
   password='jUutjnOIRlwqKFGW7ho3U812QUu5UcCS',
   decode_responses=True)
 
-#create a new user
+# create a new user
 def create_user(username):
     r.hset(username,mapping={'name':"",
-                        "phone":"",
-                        "email":"",
-                        "linkedin":"",
-                        "summary":"",
-                        "skills":"",
-                        "education":"",
-                        "experience":"",
-                        "projects":"",
-                        "extracurricular":""})
+                        'phone':"",
+                        'email':"",
+                        'linkedin':"",
+                        'summary':'',
+                        'skills':'',
+                        'education':'',
+                        'experience':'',
+                        'projects':'',
+                        'extracurricular':''})
+
+# set name, phone, email, or linkedin;
+# example: set_info("jack133003", "name", "Lebron James")
+def set_info(username, field, value):
+    r.hset(username,field,value)
+
 
 #example
 create_user("jack133003")
+set_info("jack133003", "name", "Lebron James")
+set_info("jack133003", "phone", "6471234455")
