30258c238b7823fcd6b267978778da12eb2b1371
Kevin Le <let36@mcmaster.ca>
2024-01-13 19:48:12 -0500

    added functions to add summary, add job and get info

--- db/db.py
+++ db/db.py
@@ -6,26 +6,49 @@ r = redis.Redis(
   password='jUutjnOIRlwqKFGW7ho3U812QUu5UcCS',
   decode_responses=True)
 
-# create a new user
+# create a new user, username must be unique
 def create_user(username):
-    r.hset(username,mapping={'name':"",
+    r.hset(username, mapping={'name':"",
                         'phone':"",
                         'email':"",
                         'linkedin':"",
-                        'summary':'',
-                        'skills':'',
-                        'education':'',
-                        'experience':'',
-                        'projects':'',
-                        'extracurricular':''})
+                        'summary':username+'_summary',
+                        'skills':username+'_skills',
+                        'education':username+'_educations',
+                        'experience':username+'_jobs',
+                        'projects':username+'_projects',
+                        'extracurricular':username+'_extracurricular'})
 
 # set name, phone, email, or linkedin;
 # example: set_info("jack133003", "name", "Lebron James")
 def set_info(username, field, value):
-    r.hset(username,field,value)
+    r.hset(username, field, value)
 
+# get name, phone, email, or linkedin;
+def get_info(username, field):
+    return r.hget(username, field)
+
+# add a sentence to the summary set (set of sentences)
+def add_summary(username, sentence):
+    r.sadd(username+'_summary', sentence)
+
+# add a new job, job_key must be unique between all users
+def add_job(username, job_key):
+    r.sadd(username+'_jobs', job_key)
+    r.hset(job_key, mapping={'position':"",
+                             'company':"",
+                             'location':"",
+                             'start date':"",
+                             'end date':"",
+                             'skills':job_key+'_skills',
+                             'sentences':job_key+'_sentences'})
 
 #example
 create_user("jack133003")
 set_info("jack133003", "name", "Lebron James")
 set_info("jack133003", "phone", "6471234455")
+print(get_info("jack133003","name"))
+add_summary("jack133003","I am in 3rd year")
+add_summary("jack133003", "I am in Electrical Engineering")
+add_job("jack133003", "jack133003_job1")
+add_job("jack133003", "jack133003_job2")
