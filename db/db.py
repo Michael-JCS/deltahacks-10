dc6da82b2fc6379ad6d922710f4432421d2ab826
Kevin Le <let36@mcmaster.ca>
2024-01-13 20:19:57 -0500

    added functions to set job info and get job info

--- db/db.py
+++ db/db.py
@@ -19,7 +19,7 @@ def create_user(username):
                         'projects':username+'_projects',
                         'extracurricular':username+'_extracurricular'})
 
-# set name, phone, email, or linkedin;
+# add name, phone, email, or linkedin;
 # example: set_info("jack133003", "name", "Lebron James")
 def set_info(username, field, value):
     r.hset(username, field, value)
@@ -28,9 +28,9 @@ def set_info(username, field, value):
 def get_info(username, field):
     return r.hget(username, field)
 
-# add a sentence to the summary set (set of sentences)
-def add_summary(username, sentence):
-    r.sadd(username+'_summary', sentence)
+# add a line to the summary set (set of sentences)
+def add_summary(username, line):
+    r.sadd(username+'_summary', line)
 
 # add a new job, job_key must be unique between all users
 def add_job(username, job_key):
@@ -41,14 +41,36 @@ def add_job(username, job_key):
                              'start date':"",
                              'end date':"",
                              'skills':job_key+'_skills',
-                             'sentences':job_key+'_sentences'})
+                             'description':job_key+'_description'})
+
+# set position, company, location, start date, or end date;
+# example: set_job_info("jack133003_job1", "company", "Intel")
+def set_job_info(job_key, field, value):
+    r.hset(job_key, field, value)
+
+# get position, company, location, start date, or end date;
+def get_job_info(job_key, field):
+    return r.hget(job_key, field)
 
 #example
 create_user("jack133003")
+
 set_info("jack133003", "name", "Lebron James")
 set_info("jack133003", "phone", "6471234455")
+set_info("jack133003", "email", "lebron.james@gmail.com")
+set_info("jack133003", "linkedin", "linkedin.com/in/lebron-james")
+
 print(get_info("jack133003","name"))
+
 add_summary("jack133003","I am in 3rd year")
 add_summary("jack133003", "I am in Electrical Engineering")
+
 add_job("jack133003", "jack133003_job1")
-add_job("jack133003", "jack133003_job2")
+
+set_job_info("jack133003_job1", "company", "Intel")
+set_job_info("jack133003_job1", "position", "Electrical Engineer Co-op")
+set_job_info("jack133003_job1", "start date", "May 2022")
+set_job_info("jack133003_job1", "end date", "September 2023")
+set_job_info("jack133003_job1", "location", "Toronto, ON")
+
+print(get_job_info("jack133003_job1", "position"))
